40bed1c126a48a07392e625c187625e7f13fe8bf
Serah Nderi <sarahngima77@gmail.com>
2024-03-27 16:34:54 +0300

    Fixing Cart Logic

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -36,12 +36,12 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()=== 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount(true)+2}</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : 2}</b>
             </div>
             </div>
             <button  >PROCEED TO PAYMENT</button>
